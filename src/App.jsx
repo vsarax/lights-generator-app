810596ea89adc108a6bfc421c2121a0bfaf54dfe
Sarka <saraszewczykk@wp.pl>
2023-07-15 19:13:40 +0200

    Generate grid dinamically

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,59 @@
+import React, { useState } from 'react';
 import "./App.css";
 import Counter from './components/Counter';
 import SingleLight from "./components/SingleLight";
 import Text from './components/Text';
+import {Grid} from './components/Grid';
 
 function App() {
+
+  const [rows, setRows] = useState(0);
+  const [columns, setColumns] = useState(0);
+
+  const totalLights = rows * columns;
+
+  const lightsArray = Array.from({ length: totalLights });
+
+  const handleDecrementRows = () => {
+    if( rows <= 0) {
+        return;
+    }
+    setRows(rows - 1);
+  }
+
+  const handleIncrementRows = () => {
+    setRows(rows + 1);
+  }
+
+  const handleDecrementColumns = () => {
+    if( columns <= 0) {
+        return;
+    }
+    setColumns(columns - 1);
+  }
+
+  const handleIncrementColumns = () => {
+    if (columns >= 9) {
+      return;
+    }
+    setColumns(columns + 1);
+  }
+
   
   return (
     <div className = {'content'}>
       <nav className = {'nav'}>
-        <Text> 0 bulbs</Text>
+        <Text> {totalLights} bulbs</Text>
         <div className= {'counter'}>
-            <Counter count = {0} item = {'rows'}/>
-            <Counter count = {0} item = {'columns'}/>
+            <Counter count = {rows} item = {'rows'} onIncrement = {handleIncrementRows} onDecrement = {handleDecrementRows}/>
+            <Counter count = {columns} item = {'columns'} onIncrement = {handleIncrementColumns} onDecrement = {handleDecrementColumns}/>
         </div>
       </nav>
-      <div>
-        <SingleLight/>
-      </div>
+      <Grid columns={columns}>
+        {lightsArray.map((value, index) => {
+          return <SingleLight key={index}/>;
+        })}
+      </Grid>
     </div>
   )
 };
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import IconMinus from './icons/IconMinus';
 import IconPlus from './icons/IconPlus';
 import Text from './Text';
@@ -7,25 +7,12 @@ import styles from './Counter.module.css'
 
 const Counter = (props) => {
 
-    const [value, setValue] = useState(props.count);
-
-    const onMinusHandleClick = () => {
-        if( value <= 0) {
-            return;
-        }
-        setValue(value - 1);
-    }
-
-    const onPlusHandleClick = () => {
-        setValue(value + 1);
-    }
-
     return (
         <div className={styles.wrapper}>
-          <Text>{value} {props.item}</Text>
+          <Text>{props.count} {props.item}</Text>
           <nav>
-            <IconMinus className={styles.icon} onClick={onMinusHandleClick} />
-            <IconPlus className={styles.icon} onClick={onPlusHandleClick} />
+            <IconMinus className={styles.icon} onClick={props.onDecrement} />
+            <IconPlus className={styles.icon} onClick={props.onIncrement} />
           </nav>
         </div>
       );
